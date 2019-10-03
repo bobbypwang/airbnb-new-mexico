7607b621fb066036b029b93149313796c56d6c12
Bobby Wang <bobby.p.wang@gmail.com>
2019-10-03 16:11:30 -0400

    Initial load-in of react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import './App.scss';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          Mexico City Airbnb App here
+        </div>
+      </div>
+    )
+  }
+}
