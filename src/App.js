5f35b90d103e94e3899a26bf0d06499da85cd8f4
Bobby Wang <bobby.p.wang@gmail.com>
2019-10-04 07:46:58 -0400

    Components and simple routing added for needed pages.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react'
+import { Route, Switch, Link} from "react-router-dom";
 
 import './App.scss';
+import Home from './components/Home';
+import SearchResults from './components/SearchResults';
+import Listing from './components/Listing';
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="container">
-          Mexico City Airbnb App here
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/search?s=something'>Fake Search Term "something"</Link></li>
+            <li><Link to='/listing/12345'>Individual Listing</Link></li>
+          </ul>
+        </div>
+        <div className="container">
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/listing/:id' component={Listing} />
+            <Route path='/search' component={SearchResults} />
+          </Switch>
         </div>
       </div>
     )
